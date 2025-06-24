a8b051d48f726f1e6414ea8566091cb9a130f5dd
seoyoun8694 <chaeseoyoun8694@gmail.com>
2025-06-24 15:40:30 +0900

    feat. 실습 자료

---
+++ src/main/java/com/mini2/api_gateway/filter/AuthenticationHeaderFilterFunction.java
@@ -0,0 +1,26 @@
+package com.mini2.api_gateway.filter;
+
+import com.mini2.api_gateway.security.jwt.authentication.UserPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.servlet.function.ServerRequest;
+
+import java.util.function.Function;
+
+public class AuthenticationHeaderFilterFunction {
+    public static Function<ServerRequest, ServerRequest> addHeader() {
+        return request -> {
+            ServerRequest.Builder requestBuilder = ServerRequest.from(request);
+
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if( principal instanceof UserPrincipal userPrincipal) {
+                requestBuilder.header("X-Auth-UserId",userPrincipal.getUserId());
+
+                // 다른 Claims 들도 ...
+            }
+
+            // Client ID, DeviceType 등도 필요시 입력
+
+            return requestBuilder.build();
+        };
+    }
+}
